0f1f66e3789099a012876164702dce79dc67ddfd
Kevin Ladan <kevinladan@outlook.com>
2019-10-27 01:10:18 +0800

    (MainActivity) Get rid of layout files

--- app/src/main/java/github/com/kebbbnnn/lightgallery/activity/MainActivity.java
+++ app/src/main/java/github/com/kebbbnnn/lightgallery/activity/MainActivity.java
@@ -3,7 +3,11 @@ package github.com.kebbbnnn.lightgallery.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.GridView;
+
+import org.lucasr.smoothie.AsyncGridView;
 
 import github.com.kebbbnnn.lightgallery.R;
 import github.com.kebbbnnn.lightgallery.presenter.ActionbarPresenter;
@@ -11,6 +15,7 @@ import github.com.kebbbnnn.lightgallery.presenter.GalleryPresenter;
 
 import static github.com.kebbbnnn.lightgallery.activity.ImageDisplayActivity.IMAGE_URI_ID;
 import static github.com.kebbbnnn.lightgallery.presenter.ActionbarPresenter.AB_TITLE;
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 public class MainActivity extends BaseActivity implements AdapterView.OnItemClickListener {
 
@@ -21,8 +26,24 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemClic
 
   @Override
   public void onCreateUI(Bundle savedInstanceState) {
-    setContentView(R.layout.activity_main);
+    setContentView(getView());
+
     new GalleryPresenter(this).attach(this).load();
+  }
+
+  private View getView() {
+    AsyncGridView grid = new AsyncGridView(this);
+    grid.setId(R.id.grid);
+    grid.setColumnWidth((int) getResources().getDimension(R.dimen.image_width));
+    grid.setHorizontalSpacing(1);
+    grid.setNumColumns(GridView.AUTO_FIT);
+    grid.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
+    grid.setVerticalSpacing(1);
+
+    ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT);
+    grid.setLayoutParams(params);
+
+    return grid;
   }
 
   @Override
